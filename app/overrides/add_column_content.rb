cbc25e87a7f871a4f71ea43c2c463a16e106cdc0
jmh <jmhudry@naver.com>
2018-05-08 06:59:31 +0900

    Issue #46721 - add remove
    Change-Id: Ib31d790aea524f4e47d41bca61f48e07921f65e7

--- app/overrides/add_column_content.rb
+++ app/overrides/add_column_content.rb
@@ -1,11 +1,20 @@
 if SETTINGS[:column_view]
   SETTINGS[:column_view].reject { |k,v| v[:view] && v[:view] != :hosts_list }.keys.sort.map do |k|
+    remove = SETTINGS[:column_view][k.to_sym][:remove]
     after = SETTINGS[:column_view][k.to_sym][:after]
-    Deface::Override.new(
-      :virtual_path => "hosts/_list",
-      :name => "content_#{k}",
-      :insert_after => "td:contains('#{after}')",
-      :text => "\n    <td class=\"hidden-tablet hidden-phone ellipsis\"><%= fcv_content host, '#{k}' %></td>"
-    )
+    if remove != nil
+      Deface::Override.new(
+        :virtual_path => "hosts/_list",
+        :name => "content_#{k}",
+        :remove => "td:contains('#{remove}')"
+      )
+    elsif after != nil
+      Deface::Override.new(
+        :virtual_path => "hosts/_list",
+        :name => "content_#{k}",
+        :insert_after => "td:contains('#{after}')",
+        :text => "\n    <td class=\"hidden-tablet hidden-phone ellipsis\"><%= fcv_content host, '#{k}' %></td>"
+      )
+    end
   end
 end
--- app/overrides/add_column_title.rb
+++ app/overrides/add_column_title.rb
@@ -1,12 +1,21 @@
 if SETTINGS[:column_view]
   SETTINGS[:column_view].reject { |k,v| v[:view] && v[:view] != :hosts_list }.keys.sort.map do |k|
+    remove = SETTINGS[:column_view][k.to_sym][:remove]
     after = SETTINGS[:column_view][k.to_sym][:after]
     width = SETTINGS[:column_view][k.to_sym][:width] || "10%"
-    Deface::Override.new(
-      :virtual_path => "hosts/_list",
-      :name => "title_#{k}",
-      :insert_after => "th:contains('#{after}')",
-      :text => "\n    <th class=\"hidden-tablet hidden-phone\" width=\"#{width}\"><%= fcv_title '#{k}' %></th>"
-    )
+    if remove != nil
+      Deface::Override.new(
+        :virtual_path => "hosts/_list",
+        :name => "title_#{k}",
+        :remove => "th:contains('#{remove}')"
+      )
+    elsif after != nil
+      Deface::Override.new(
+        :virtual_path => "hosts/_list",
+        :name => "title_#{k}",
+        :insert_after => "th:contains('#{after}')",
+        :text => "\n    <th class=\"hidden-tablet hidden-phone\" width=\"#{width}\"><%= fcv_title '#{k}' %></th>"
+      )
+    end
   end
 end
